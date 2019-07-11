87b843d25d3ea7731d8de6a07588372091870350
Bogomolov Mikhail <mocrobs@gmail.com>
2019-07-11 12:08:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testourtabs/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.testourtabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        TabAdapter adapter = new TabAdapter(this);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(adapter.getTabView(i));
+        }
+    }
+}
